027e0d344cb20dab48ade38a59167c061cda77ff
Saurabh <saurabhb7678@gmail.com>
2024-01-10 19:13:24 +0530

    10JAN 	modified:   index.html

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,27 +1,49 @@
 const paper = document.querySelector("#paper"),
   pen = paper.getContext("2d");
 console.log("Load");
-
+let startTime = new Date().getTime();
 const draw = () => {
+  const currentTime = new Date().getTime(),
+    elapsedTime = (currentTime - startTime) / 1000;
+
+  // console.log(elapsedTime);
   paper.width = paper.clientWidth;
   paper.height = paper.clientHeight;
 
   const start = {
     x: paper.width * 0.1,
-    y: paper.height * 0.9,
+    y: paper.height * 0.5,
   };
 
   const end = {
     x: paper.width * 0.9,
-    y: paper.height * 0.9,
+    y: paper.height * 0.5,
   };
 
-  pen.strokeStyle = "red";
-  pen.lineWidth = 6;
+  const center = {
+    x: paper.width * 0.5,
+    y: paper.height * 0.5,
+  };
+
+  const arcradius = 5,
+    velocity = 10;
+  (distance = Math.PI + elapsedTime * velocity), (length = 20);
+  const x = center.x + length * Math.cos(distance),
+    y = center.x + length * Math.sin(distance);
+
+  // pen.strokeStyle = "black";
+  // pen.lineWidth = 5;
+  // pen.beginPath();
+  // pen.moveTo(start.x, start.y);
+  // pen.lineTo(end.x, end.y);
+  // pen.stroke();
+
+  pen.fillStyle = "black";
   pen.beginPath();
-  pen.movetTo(start.x, start.y);
-  pen.lineTo(end.x, end.y);
+  pen.arc(x, y, arcradius, 0, 2 * Math.PI);
   pen.stroke();
+  pen.fill();
+  requestAnimationFrame(draw);
 };
 
 draw();
